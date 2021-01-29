c62b3eb35d0c630b925a79b749218bd08ece8815
dae4 <3210m0123@gmail.com>
2021-01-29 21:59:12 +0900

    ...

--- Day_06_BitOperation.py
+++ Day_06_BitOperation.py
@@ -1,3 +1,4 @@
+#%%
 import cv2
 import numpy as np
 
@@ -10,19 +11,21 @@ w,h,c=img2.shape
 img1 = cv2.resize(img1,dsize=(h,w))
 print(img1.shape)
 
-roi = img2[0;row,0:cols]
+roi = img2[0:w,0:h]
 
 img2gray = cv2.cvtColor(img1,cv2.COLOR_BGR2GRAY)
-ret, mask = cv2.threshold(img2gray,10,255,cv2.THRESH_BINARY)
+ret, mask = cv2.threshold(img2gray,150,255,cv2.THRESH_BINARY)
 mask_inv = cv2.bitwise_not(mask)
 
 img1_fg = cv2.bitwise_and(img1,img1,mask=mask)
-img2_bg = cv2.bitwise_and(rot,roi,mask=mask_inv)
+img2_bg = cv2.bitwise_and(roi,roi,mask=mask_inv)
 
 dst = cv2.add(img1_fg,img2_bg)
 
 img2[0:w,0:h]=dst
 
-cv2.imsh0ow('res',img2)
+cv2.imshow('res',img2)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+# %%
